18e01eac93abf40f99f5e31b2ebc41a1abb2a4fd
Robert Ancell <robert.ancell@gmail.com>
2018-10-04 22:58:42 +1300

    Attempt to decode LZMA

--- microbit-decode.c
+++ microbit-decode.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <lzma.h>
 #include <glib.h>
 
 static int
@@ -69,24 +70,20 @@ decode_integer_le (const gchar *text, int offset, int length)
 #define HEADER_LENGTH 9
 #define TRAILER_LENGTH 2
 
-int main (int argc, char **argv)
+static gboolean
+decode_hex_file (const gchar *filename, gchar **header, GBytes **source)
 {
-    if (argc < 2) {
-        g_printerr ("%s [filename.hex]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
     g_autofree gchar *contents = NULL;
     g_autoptr(GError) error = NULL;
-    if (!g_file_get_contents (argv[1], &contents, NULL, &error)) {
+    if (!g_file_get_contents (filename, &contents, NULL, &error)) {
         g_printerr ("Failed to load file: %s\n", error->message);
-        return EXIT_FAILURE;
+        return FALSE;
     }
    
     g_autofree gchar *json_header = NULL;
     guint32 json_header_address = 0;
     gsize json_header_length = 0;
-    g_autofree gchar *text = NULL;
+    g_autofree guint8 *text = NULL;
     guint32 text_address = 0;
     gsize text_length = 0;   
 
@@ -99,17 +96,17 @@ int main (int argc, char **argv)
 
        if (record[0] != ':') {
            g_printerr ("Unexpected character '%c'\n", isprint (record[0]) ? record[0] : '?');
-           return EXIT_FAILURE;
+           return FALSE;
        }
        if (strlen (record) < HEADER_LENGTH) {
            g_printerr ("No enough space for record\n");
-           return EXIT_FAILURE;
+           return FALSE;
        }
        int byte_count = decode_byte (record, 1);
        int record_length = HEADER_LENGTH + byte_count * 2 + TRAILER_LENGTH;
        if (strlen (record) < record_length) {
            g_printerr ("No enough space for record\n");
-           return EXIT_FAILURE;
+           return FALSE;
        }
        int address = address_offset + decode_integer_be (record, 3, 2);
        int record_type = decode_byte (record, 7);
@@ -132,50 +129,102 @@ int main (int argc, char **argv)
            }
 
            /* PXT file data */
-           if (address >= json_header_address && address < json_header_address + json_header_length) {
-               int n = byte_count;
-               int n_remaining = json_header_address + json_header_length - address;
-               if (n > n_remaining)
-                   n = n_remaining;
-               for (int i = 0; i < n; i++) {
-                   int byte = decode_byte (record_data, i * 2);
-                   json_header[address - json_header_address + i] = byte;
-               }
-           }
-           if (address >= text_address && address < text_address + text_length) {
-               int n = byte_count;
-               int n_remaining = text_address + text_length - address;
-               if (n > n_remaining)
-                   n = n_remaining;
-               for (int i = 0; i < n; i++) {
-                   int byte = decode_byte (record_data, i * 2);
-                   text[address - text_address + i] = byte;
-               }
+           for (int i = 0; i < byte_count; i++) {
+               int byte = decode_byte (record_data, i * 2);
+               guint32 a = address + i;
+               if (a >= json_header_address && a < json_header_address + json_header_length)
+                   json_header[a - json_header_address] = byte;
+               if (a >= text_address && a < text_address + text_length)
+                   text[a - text_address] = byte;
            }
            break;
        case 1:
-           g_print ("%s\n", json_header);
-           g_file_set_contents ("output", text, text_length, NULL);
-           return EXIT_SUCCESS;
+           if (header != NULL)
+               *header = g_steal_pointer (&json_header);
+           if (source != NULL)
+               *source = g_bytes_new_take (g_steal_pointer (&text), text_length);
+           return TRUE;
        case 4:
            if (byte_count != 2) {
                g_printerr ("Extended linear address wrong size (%d, expecting 2)\n", byte_count);
-               return EXIT_FAILURE;
+              return FALSE;
            }
            address_offset = decode_integer_be (record_data, 0, 2) << 16;
            break;
        case 5:
            if (byte_count != 4) {
                g_printerr ("Start linear address wrong size (%d, expecting 4)\n", byte_count);
-               return EXIT_FAILURE;
+               return FALSE;
            }
            start_address = decode_integer_be (record_data, 0, 4);
            //g_print ("START %08X\n", start_address);
            break;
        default:
            g_printerr ("Unknown record type %d\n", record_type);
+           break;
        }
     }
+}
+
+static GBytes *
+lzma_decode (GBytes *input)
+{
+    lzma_stream stream = LZMA_STREAM_INIT;
+    lzma_filter filters[2];
+    lzma_options_lzma options;
+    lzma_lzma_preset (&options, LZMA_PRESET_DEFAULT);
+    filters[0].id = LZMA_FILTER_LZMA1;
+    filters[0].options = &options;
+    filters[1].id = LZMA_VLI_UNKNOWN;
+    lzma_ret ret = lzma_raw_decoder (&stream, filters);
+    if (ret != LZMA_OK) {
+        g_printerr ("Failed to initialize LZMA decoder\n");
+        return NULL;
+    }
+    gsize data_length;
+    const uint8_t *data = g_bytes_get_data (input, &data_length);
+    stream.next_in = data;
+    stream.avail_in = data_length;
+    g_autoptr(GByteArray) output = g_byte_array_sized_new (data_length * 2);
+    while (TRUE) {
+        guint8 buffer[1024];
+
+        stream.next_out = buffer;
+        stream.avail_out = 1024;
+        int ret = lzma_code (&stream, LZMA_RUN);
+        if (ret == LZMA_STREAM_END) {
+            g_byte_array_ref (output);
+            return g_byte_array_free_to_bytes (output);
+        }
+        if (ret != LZMA_OK) {
+            g_printerr ("LZMA decoder error %d\n", ret);
+            return NULL;
+        }
+        g_byte_array_append (output, buffer, 1024 - stream.avail_out);
+    }
+}
+
+int
+main (int argc, char **argv)
+{
+    if (argc < 2) {
+        g_printerr ("%s [filename.hex]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    g_autofree gchar *header = NULL;
+    g_autoptr(GBytes) source = NULL;
+    if (!decode_hex_file (argv[1], &header, &source))
+        return EXIT_FAILURE;
+
+    g_file_set_contents ("output", g_bytes_get_data (source, NULL), g_bytes_get_size (source), NULL);
+
+    g_autoptr(GBytes) decoded_source = lzma_decode (source);
+    if (decoded_source == NULL)
+        return EXIT_FAILURE;
+
+    g_print ("%s\n", header);
+    g_print ("%.*s\n", (int) g_bytes_get_size (decoded_source), (gchar *) g_bytes_get_data (decoded_source, NULL));
 
     return EXIT_SUCCESS;
 }
