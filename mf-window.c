f96c022eb28a1667af8a932fc9448d6db4830908
Robert Ancell <robert.ancell@gmail.com>
2018-10-06 22:22:36 +1300

    Actually write the image to the micro:bit!

--- mf-window.c
+++ mf-window.c
@@ -21,8 +21,10 @@ struct _MfWindow
     GtkLabel     *not_detected_label;
     GtkImage     *device_image;
 
+    GCancellable *cancellable;
     MbMonitor    *monitor;
     MbFile       *file;
+    MbDevice     *device;
 };
 
 G_DEFINE_TYPE (MfWindow, mf_window, GTK_TYPE_WINDOW)
@@ -30,6 +32,8 @@ G_DEFINE_TYPE (MfWindow, mf_window, GTK_TYPE_WINDOW)
 void
 mf_window_init (MfWindow *self)
 {
+    self->cancellable = g_cancellable_new ();
+
     gtk_widget_init_template (GTK_WIDGET (self));
 }
 
@@ -38,16 +42,42 @@ mf_window_dispose (GObject *object)
 {
     MfWindow *self = MF_WINDOW (object);
 
+    g_cancellable_cancel (self->cancellable);
+
+    g_clear_object (&self->cancellable);
     g_clear_object (&self->monitor);
     g_clear_object (&self->file);
+    g_clear_object (&self->device);
 
     G_OBJECT_CLASS (mf_window_parent_class)->dispose (object);
 }
 
 static void
-flash_cb (MfWindow *window)
+flash_copied_cb (GObject *object, GAsyncResult *result, gpointer user_data)
 {
-    g_printerr ("flash\n");
+    //MfWindow *self = user_data;
+
+    g_autoptr(GError) error = NULL;
+    if (!g_file_copy_finish (G_FILE (object), result, &error)) {
+        if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+           g_warning ("Failed to flash file: %s", error->message);
+        return;
+    }
+}
+
+static void
+flash_cb (MfWindow *self)
+{
+    g_autofree gchar *basename = g_file_get_basename (mb_file_get_file (self->file));
+    g_autoptr(GFile) target = g_file_get_child (g_mount_get_root (mb_device_get_mount (self->device)), basename);
+
+    g_file_copy_async (mb_file_get_file (self->file),
+                       target,
+                       G_FILE_COPY_NONE,
+                       G_PRIORITY_DEFAULT,
+                       self->cancellable,
+                       NULL, NULL,
+                       flash_copied_cb, self);
 }
 
 void
@@ -71,9 +101,12 @@ mf_window_class_init (MfWindowClass *klass)
 static void
 devices_changed_cb (MfWindow *self)
 {
-    gboolean have_devices = mb_monitor_get_devices (self->monitor)->len > 0;
-    gtk_widget_set_sensitive (GTK_WIDGET (self->flash_button), self->file != NULL && have_devices);
-    gtk_stack_set_visible_child (self->device_stack, have_devices ? GTK_WIDGET (self->device_image) : GTK_WIDGET (self->not_detected_label));
+    g_clear_object (&self->device);
+    GPtrArray *devices = mb_monitor_get_devices (self->monitor);
+    if (devices->len > 0)
+        self->device = g_object_ref (g_ptr_array_index (devices, 0));
+    gtk_widget_set_sensitive (GTK_WIDGET (self->flash_button), self->file != NULL && self->device != NULL);
+    gtk_stack_set_visible_child (self->device_stack, self->device != NULL ? GTK_WIDGET (self->device_image) : GTK_WIDGET (self->not_detected_label));
 }
 
 MfWindow *
@@ -97,4 +130,6 @@ mf_window_set_file (MfWindow *self, MbFile *file)
     if (self->file != NULL)
         title = mb_file_get_name (self->file);
     gtk_header_bar_set_title (self->header_bar, title);
+
+    devices_changed_cb (self);
 }
