652ad91b4b89f960fdd28f6e08c86c12b31ff51c
Robert Ancell <robert.ancell@gmail.com>
2018-10-06 20:27:53 +1300

    Add copyright / license

--- mb-monitor.h
+++ mb-monitor.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2018 Robert Ancell
+ * Author: Robert Ancell <robert.ancell@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+ * license.
+ */
+
 #pragma once
 
 #include <glib-object.h>
